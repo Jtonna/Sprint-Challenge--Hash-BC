ac8360f26b8eb642784827b9b68c96954e420a5a
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-06 11:42:01 -0800

    This should work but its not

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -35,6 +35,19 @@ def reconstruct_trip(tickets, length):
 
     """
 
+    # Insert each ticket into the hash table
+    for individual_ticket in range(length):
+        flying_from = tickets[individual_ticket].source
+        flying_to = tickets[individual_ticket].destination
+        hash_table_insert(hashtable, flying_from, flying_to)
     
+    # Finds the ticket with its source as "None", since its the origin ticket
+    route[0] = hash_table_retrieve(hashtable, "None")
+
+    # Loop over the rest of the table
+    for each_ticket in range(1, length):
+        # Since we already have the origin ticket we can just subtract 1 from it to get the key of the next ticket
+        # we then save that as the next ticket in the route
+        route[each_ticket] = hash_table_retrieve(hashtable, route[each_ticket] - 1)
 
     return route
