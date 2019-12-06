3e345d8ed530d0d6bb17a6900e9a912f10bb7e34
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-06 10:58:05 -0800

    passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -20,8 +20,22 @@ def get_indices_of_item_weights(weights, length, limit):
         if the weight is not None
             return the weight
 
+    hashtable = ht
+    key = weights[item]
+    value = item
     """
 
+    for item in range(len(weights)):
+        hash_table_insert(ht, weights[item], item)
+    
+    for item in range(len(weights)):
+        subtractifier = limit - weights[item] # weights[item] is the key
+        checker = hash_table_retrieve(ht, subtractifier)
+
+        if checker is not None:
+            return (checker, item)
+        
+    return None
 
 
 def print_answer(answer):
